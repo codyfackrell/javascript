24d0a8f77d9adc4673b09c591507afeb9ee0b2a0
Cody Fackrell <codyfackrell56@gmail.com>
2025-09-27 21:22:28 -0600

    updated files

---
+++ script.js
@@ -0,0 +1,26 @@
+let vocab = []
+
+// console.log("hello", "world", 3)
+// console.error("dawg...")
+// console.warn("heads up bro")
+
+vocab.push('npm')
+
+// const v = 48
+
+// console.log(`Jimmie is #${v}`)
+
+
+// const prompt = require("prompt-sync")()
+
+// const v = prompt("Type Something") 
+
+// console.log(v)
+
+// nule = when you want to set something to nothing
+// undefined = when you havent assigned a value to something
+
+// BigInt = a data type that store big integers that are not supported by the Number data type
+vocab.push('BigInt', 'function-scoped', 'block-scoped')
+
+console.log(vocab)
